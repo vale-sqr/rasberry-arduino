3dc2808b3a90fcfdd7e9caa1b0fe4ec694636a3d
vale <zhanna.nazarch@gmail.com>
2026-06-06 09:30:41 -0700

    Update joint_library.py

--- joint_library.py
+++ joint_library.py
@@ -1,9 +1,9 @@
-joint_commands = {'neck__longitudinal': ['up_full', 'up_slight', 'forward', 'down_slight', 'down_full'], 
-'neck_turn': ['left_full', 'left_slight', 'neutral', 'right_slight', 'right_full'],
-'neck_tilt': ['left_full', 'left_slight', 'neutral', 'right_slight', 'right_full'],
-'shoulder_left_longitudinal_rise': ['up', 'forward_up', 'forward', 'forward_down', 'down', 'backward_down', 'backward_up'],
-'shoulder_left_side_rise': ['down', 'extended_down', 'extended', 'extended_up', 'up'],
-'shoulder_right_longitudina_rise': ['up', 'forward_up', 'forward', 'forward_down', 'down', 'backward_down', 'backward_up'],
-'shoulder_right_side_rise': ['down', 'extended_down', 'extended', 'extended_up', 'up'],
-'elbow_left': ['full_open', 'almost_open', 'right_angle', 'slight_open', 'closed'],
-'elbow_right': ['full_open', 'almost_open', 'right_angle', 'slight_open', 'closed']}
+joint_commands = {'neck__longitudinal': {'up_full': 0, 'up_slight': 45, 'forward': 90, 'down_slight': 135, 'down_full': 180}, 
+'neck_turn': {'left_full': 0, 'left_slight': 45, 'neutral': 90, 'right_slight': 135, 'right_full': 180},
+'neck_tilt': {'left_full': 0, 'left_slight': 45, 'neutral': 90, 'right_slight': 135, 'right_full': 180},
+'shoulder_left_longitudinal_rise': {'up': 0, 'forward_up': 30, 'forward': 60, 'forward_down': 90, 'down': 120, 'backward_down': 150, 'backward_up': 180},
+'shoulder_left_side_rise': {'down': 0, 'side_down': 45, 'side': 90, 'side_up': 135, 'up': 180},
+'shoulder_right_longitudinal_rise': {'up': 0, 'forward_up': 30, 'forward': 60, 'forward_down': 90, 'down': 120, 'backward_down': 150, 'backward_up': 180},
+'shoulder_right_side_rise': {'down': 0, 'side_down': 45, 'side': 90, 'side_up': 135, 'up': 180},
+'elbow_left': {'full_open': 0, 'almost_open': 45, 'right_angle': 90, 'slight_open': 135, 'closed': 180},
+'elbow_right': {'full_open': 0, 'almost_open': 45, 'right_angle': 90, 'slight_open': 135, 'closed': 180}
