18b224b7ed6b8b683dbc9b7cbf3581454635ed8a
vale <zhanna.nazarch@gmail.com>
2026-06-13 10:44:02 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,48 @@
+pip install -U discord.py
+
+import os
+import discord
+from dotenv import load_dotenv
+
+import ollama
+import re
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.CLIENT()
+
+# .env
+# DISCORD_TOKEN={your-bot-token}
+#pip install -U python-dotenv
+
+ai_authors = ["names of all of the ai's talking"]
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+  if message.author in ai_authors:
+    matches - re.findall(r'\*([^*]+)\*', message.content)
+    for expression in matches:
+      response = ollama.chat(
+        model = "llama3.2",
+        messages = [
+          {"role": "system", "content": "my super cool and awesome system prompt"},
+          {"role", "user", "content": expression},
+        ]
+      )
+
+      joint_commnd = response['message']['content'] 
+      #format how I need to send the message
+
+
+
+
+
+
+      
+    
+    
